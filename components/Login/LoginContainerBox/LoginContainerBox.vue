bda2bf414774c051899160fa38a784ff997a1aa4
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-04 17:21:48 +0800

    0.2.3

--- components/Login/LoginContainerBox/LoginContainerBox.vue
+++ components/Login/LoginContainerBox/LoginContainerBox.vue
@@ -15,11 +15,5 @@ export default{
 }
 </script>
 <style scoped lang="sass">
-.main
-    animation: login-container-box-animation 2s ease
-@keyframes login-container-box-animation
-  0%
-    left: -100%
-  100%
-    left: 0%
+@import "../sass/main.sass"
 </style>
