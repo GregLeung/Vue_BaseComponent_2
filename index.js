88fccaf7e4acd0c309125a5d49e6441af9120341
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-04 18:42:15 +0800

    0.2.7

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-import BaseFooter from "./components/BaseFooter/BaseFooter.vue"
-import BaseTable from "./components/BaseTable/BaseTable.vue"
-import BaseUploadButton from "./components/BaseUploadButton/BaseUploadButton.vue"
-import CMSHeader from "./components/CMSHeader/CMSHeader.vue"
-import NavigationBar from "./components/NavigationBar/NavigationBar.vue"
-import NavigationBarToggleButton from "./components/NavigationBarToggleButton/NavigationBarToggleButton.vue"
-import LoginContainerBox from "./components/Login/LoginContainerBox/LoginContainerBox.vue"
-import LoginInput from "./components/Login/LoginInput/LoginInput.vue"
-import Request from "./components/Login/LoginInput/LoginInput.vue"
+import BaseFooter from "./components/BaseFooter/BaseFooter.vue";
+import BaseTable from "./components/BaseTable/BaseTable.vue";
+import BaseUploadButton from "./components/BaseUploadButton/BaseUploadButton.vue";
+import CMSHeader from "./components/CMSHeader/CMSHeader.vue";
+import NavigationBar from "./components/NavigationBar/NavigationBar.vue";
+import NavigationBarToggleButton from "./components/NavigationBarToggleButton/NavigationBarToggleButton.vue";
+import LoginContainerBox from "./components/Login/LoginContainerBox/LoginContainerBox.vue";
+import LoginInput from "./components/Login/LoginInput/LoginInput.vue";
+import Request from "./util/request";
+import Excel from "./util/excel";
+import Util from "./util/util";
+import GoogleMap from "./util/googleMap";
 
-
-export {BaseFooter, BaseTable, BaseUploadButton, CMSHeader, NavigationBar, NavigationBarToggleButton, LoginContainerBox, LoginInput};
+export {
+  BaseFooter,
+  BaseTable,
+  BaseUploadButton,
+  CMSHeader,
+  NavigationBar,
+  NavigationBarToggleButton,
+  LoginContainerBox,
+  LoginInput,
+  Request,
+  Excel,
+  Util,
+  GoogleMap
+};
