3a1792472258f2a6e3eb62a736468293fa8e9ea9
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-04 16:55:48 +0800

    0.1.34

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import BaseTable from "./components/BaseTable/BaseTable.vue"
 import BaseUploadButton from "./components/BaseUploadButton/BaseUploadButton.vue"
 import CMSHeader from "./components/CMSHeader/CMSHeader.vue"
 import NavigationBar from "./components/NavigationBar/NavigationBar.vue"
+import NavigationBarToggleButton from "./components/NavigationBarToggleButton/NavigationBarToggleButton.vue"
 
 
-export {BaseFooter, BaseTable, BaseUploadButton, CMSHeader, NavigationBar};
+export {BaseFooter, BaseTable, BaseUploadButton, CMSHeader, NavigationBar, NavigationBarToggleButton};
