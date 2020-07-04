8a1c7dd2d2ad2a1922bf2c2f49fd6678ec49beef
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-04 17:03:05 +0800

    0.2.0 add login components

---
+++ components/LoginContainerBox/LoginContainBox.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main">
+      <p class="sign" align="center">{{title}}</p>
+          <slot></slot>
+    </div>
+</template>
+<script lang="js">
+export default{
+    props: {
+        title:{
+            type: String,
+            required: true,
+        } 
+    }
+}
+</script>
+<style scoped lang="sass">
+.main
+    animation: login-container-box-animation 2s ease
+@keyframes login-container-box-animation
+  0%
+    left: -100%
+  100%
+    left: 0%
+</style>
---
+++ components/LoginInput/LoginInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <input v-model="value" class="un" :type="type" align="center" :placeholder="placeholder" />
+</template>
+<script lang="js">
+export default{
+    props: {
+        placeholder: {
+            type: String,
+            default: ""
+        },
+        type: {
+            type: String,
+            default: "text"
+        }
+    },
+    data(){
+        return{
+            value: ""
+        }
+    }
+}
+</script>
+
+<style scoped lang="sass">
+.un 
+  width: 76%
+  color: rgb(38, 50, 56)
+  font-weight: 700
+  font-size: 14px
+  letter-spacing: 1px
+  background: rgba(136, 126, 126, 0.04)
+  padding: 10px 20px
+  border: none
+  border-radius: 20px
+  outline: none
+  box-sizing: border-box
+  border: 2px solid rgba(0, 0, 0, 0.02)
+  text-align: center
+  font-family: "Ubuntu", sans-serif
+.un:focus
+    border: 2px solid rgba(0, 0, 0, 0.18) !important
+</style>
