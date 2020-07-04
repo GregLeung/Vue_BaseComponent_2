704c6649dadbefce0facf5295f152aaff375fb07
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-04 17:07:17 +0800

    0.2.1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import BaseUploadButton from "./components/BaseUploadButton/BaseUploadButton.vue
 import CMSHeader from "./components/CMSHeader/CMSHeader.vue"
 import NavigationBar from "./components/NavigationBar/NavigationBar.vue"
 import NavigationBarToggleButton from "./components/NavigationBarToggleButton/NavigationBarToggleButton.vue"
+import LoginContainerBox from "./components/LoginContainerBox/LoginContainerBox.vue"
+import LoginInput from "./components/LoginInput/LoginInput.vue"
 
 
-export {BaseFooter, BaseTable, BaseUploadButton, CMSHeader, NavigationBar, NavigationBarToggleButton};
+export {BaseFooter, BaseTable, BaseUploadButton, CMSHeader, NavigationBar, NavigationBarToggleButton, LoginContainerBox, LoginInput};
