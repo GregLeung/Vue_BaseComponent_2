e8eb97d112303b3c421cb68e3541b8e335c87327
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-07-10 09:11:12 +0800

    0.5.0 add CMS Form Input

---
+++ components/CMSFormConponent/CMSFormInput/CMSFormInput.vue
@@ -0,0 +1,46 @@
+<template>
+<el-form-item :class="{'basic-information-field': (direction == 'horizontal')}">
+    <template slot="label">
+        <h1>{{label}}</h1>
+    </template>
+    <el-input v-bind:value="value" @input="handleOnChange"/>
+</el-form-item>
+</template>
+
+<script lang="js">
+import Vue from "vue";
+export default Vue.extend({
+    props: {
+        label: {
+            type: String,
+            required: true,
+            default: ""
+        },
+        value: {
+            type: String
+        },
+        direction: {
+            type: String,
+            required: false,
+            default: "horizontal"
+        }
+    },
+    model:{
+        prop: "value",
+        event: "update"
+    },
+    methods:{
+        handleOnChange(value){
+            this.$emit("update", value)
+        }
+    }
+})
+</script>
+
+<style lang="sass" scoped>
+.basic-information-field
+  display: flex
+  align-items: center
+  margin-bottom: 0px
+  width: 100%
+</style>
