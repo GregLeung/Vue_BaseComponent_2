8e26d6166fe4cabd24f681b896b2270622de9b23
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-08-05 13:06:39 +0800

    0.8.27

--- util/request.js
+++ util/request.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Util from "./util.js";
 import store from "@/store/index";
+axios.defaults.headers.common['API-KEY'] = store().state.api_key || "";
 class Request {
   static get(
     vueInstance,
