cfc9636d050513726851eea02d385c7e186bcca2
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-08-15 14:08:51 +0800

    0.10.0

---
+++ components/CMSFormComponent/CMSInputNumber/CMSInputNumber.vue
@@ -0,0 +1,61 @@
+<template>
+<div :class="{'basic-information-field': (direction == 'horizontal')}">
+    <label :style="{'min-width': labelWidth + 'em'}">
+        <h1 :style="{'font-size':  fontSize + 'rem'}">{{label}}</h1>
+    </label>
+    <el-input-number v-bind:value="value" @change="handleOnChange" :min="min" :max="max"  />
+</div>
+</template>
+
+<script lang="js">
+import Vue from "vue";
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true,
+            default: ""
+        },
+        direction: {
+            type: String,
+            required: false,
+            default: "horizontal"
+        },
+        fontSize: {
+            type: Number,
+            required: false,
+            default: 1
+        },
+        labelWidth: {
+            type: Number,
+            required: false
+        },
+        min: {
+            type: Number,
+            required: false,
+            default: 0
+        },
+        max: {
+            type: Number,
+            required: false,
+            default: 999999
+        },
+        value: {
+            type: Number,
+        },
+    },
+    model:{
+        prop: "value",
+        event: "update"
+    },
+    methods:{
+        handleOnChange(value){
+            this.$emit("update", value)
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+@import "../cmsInput.sass"
+</style>
