8a28b6cd8dedf8cfbf2f2153b182d6c5a9ce3487
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-09-07 10:30:41 +0800

    0.10.10

---
+++ components/CMSFormComponent/CMSCheckBox/CMSCheckBox.vue
@@ -0,0 +1,55 @@
+<template>
+<div :class="{'basic-information-field': (direction == 'horizontal')}">
+    <label :style="{'min-width': labelWidth + 'em'}">
+        <h1 :style="{'font-size':  fontSize + 'rem'}">{{label}}</h1>
+    </label>
+    <el-checkbox :disabled="disabled" v-bind:value="value" @input="handleOnChange" @change="$emit('change', $event)"></el-checkbox>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        label:{
+            type: String,
+            require: true,
+            default: ""
+        },
+        disabled: {
+            type: Boolean,
+            require: true,
+            default: false
+        },
+        value:{
+            type: Boolean
+        },
+        fontSize: {
+            type: Number,
+            required: false,
+            default: 1
+        },
+        labelWidth: {
+            type: Number,
+            required: false
+        },
+        direction: {
+            type: String,
+            required: false,
+            default: "horizontal"
+        },
+    },
+    model:{
+        prop: "value",
+        event: "update"
+    },
+    methods: {
+        handleOnChange(value){
+            this.$emit("update", value)
+        }
+    }
+};
+</script>
+<style lang="sass" scoped>
+@import "../cmsInput.sass"
+</style>
+
