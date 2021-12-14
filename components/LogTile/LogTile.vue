0685c634701590b9a68ac50e259447eba910c0d2
Hinorpio <hinscorpio@gmail.com>
2021-12-14 16:37:42 +0800

    0.23.13

--- components/LogTile/LogTile.vue
+++ components/LogTile/LogTile.vue
@@ -1,19 +1,19 @@
 <template>
 <div>
     <div class="row">
-        <div style="width: 25%"><el-tag effect="dark">{{fieldName}}</el-tag></div>
-        <div style="width: 25%" class="value-text-wrapper">
+        <div style="width: 30%"><el-tag effect="dark">{{fieldName}}</el-tag></div>
+        <div style="width: 20%" class="value-text-wrapper">
             <el-tooltip v-if="oldValue != null && oldValue != ''" class="item" effect="dark" :content="oldValue" placement="top-start">
                 <el-tag class="value-text" type="danger">{{oldValue}}</el-tag>
             </el-tooltip>
         </div>
         <i class="el-icon-right "></i>
-        <div style="width: 25%" class="value-text-wrapper">
+        <div style="width: 20%" class="value-text-wrapper">
             <el-tooltip v-if="newValue != null && newValue != ''" class="item" effect="dark" :content="newValue" placement="top-start">
                 <el-tag type="success">{{newValue}}</el-tag>
             </el-tooltip>
         </div>
-        <div style="width: 25%; text-align: right">
+        <div style="width: 30%; text-align: right">
             <span :style="{'font-weight': 'bold'}">{{name}}</span>
         </div>
     </div>
