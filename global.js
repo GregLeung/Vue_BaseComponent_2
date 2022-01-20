b06c2ed83cc112b8b0111d7ac231158cb98c722a
Cheng Wai Lim <s1113618@s.eduhk.hk>
2022-01-20 16:44:25 +0800

    0.24.9

---
+++ global.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import variables from "@/static/variables.scss";
+Vue.prototype.$bus = new Vue();
+Vue.mixin({
+    computed: {
+        mainTheme() {
+            return variables["main-theme"];
+        },
+        mainThemePlain() {
+            return variables["main-theme-plain"];
+        },
+        navigationBarWidth() {
+            return variables["navigation-bar-width"];
+        },
+        cmsHeaderWidth() {
+            return variables["cms-header-width"];
+        },
+        cmsHeaderWidthWithCollapseNavigationBar() {
+            return variables["cms-header-width-with-collapse-navigation-bar"];
+        },
+        viewPortRatio() {
+            if (window.innerWidth < 1024)
+                return variables["phone-width-ratio"]
+            else if (window.innerWidth < 1440)
+                return variables["small-pad-width-ratio"]
+            else if (window.innerWidth < 1920)
+                return variables["pad-width-ratio"]
+            else
+                return variables["pc-width-ratio"]
+        }
+    },
+});
